f78c31c353f25fefa33281abf41d9fc3464417b3
DenisRebenock <denmaloyreb@mail.ru>
2020-03-19 18:17:49 +0300

    refactor: add code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 
 exports.min = function min (array) {
-  let minArr = array(0);
+  let minArr = 0;
+  if (array.length==0 || !array) {
+    return 0;
+  }
+  else {
   for (let i=0; i<array.length; i++) {
     if (array[i]<minArr) {
       minArr=array[i];
     }
   }
+}
   return minArr;
 }
 
 exports.max = function max (array) {
-  let maxArr = array(0);
+  let maxArr = 0;
   for (let i=0; i<array.length; i++) {
     if (array[i]>maxArr) {
       maxArr=array[i];
