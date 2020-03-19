48190fd35273dd3f54ed7d65334f149a6c2a95a8
DenisRebenock <denmaloyreb@mail.ru>
2020-03-19 18:06:33 +0300

    refactor: add functions in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minArr = array(0);
+  for (let i=0; i<array.length; i++) {
+    if (array[i]<minArr) {
+      minArr=array[i];
+    }
+  }
+  return minArr;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxArr = array(0);
+  for (let i=0; i<array.length; i++) {
+    if (array[i]>maxArr) {
+      maxArr=array[i];
+    }
+  }
+  return maxArr;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let  avgArr;
+  let sum=0;
+  for (let i=0; i<array.length; i++) {
+    sum = sum+array[i];
+    }
+  avgArr = sum/array.length;
+  return avgArr;
 }
